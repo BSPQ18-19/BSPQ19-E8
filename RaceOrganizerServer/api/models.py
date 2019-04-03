9a997b25f8de99bf396f84d8b579d575b6b8d28c
Andrea Gallego <andrea.gallego@opendeusto.es>
2019-04-03 18:26:04 +0200

    First lines of models.py updated

--- RaceOrganizerServer/api/models.py
+++ RaceOrganizerServer/api/models.py
@@ -11,3 +11,36 @@ class User(models.Model):
     email = models.CharField(max_length=64, unique=True)
 
     isOrganizer = models.BooleanField(default=False)
+    isRunner = models.BooleanField(default=False)
+    isVolunteer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user_text
+
+    def create_race(self):
+        return self.race
+    def update_race_information(self):
+        return self.info
+    def register_as_user(self):
+        return self.user
+    def delete_from_race(self):
+        return self.race
+    def sign_up_for_a_task(self):
+        return self.task
+    def check_race_details(self):
+        return self.race
+    def set_up_for_a_race(self):
+        return self.race
+
+
+class Race(models.Model):
+    edition= models.CharField(max_length=32, unique=True)
+    sponsor = models.CharField(max_length=32)
+    place = models.CharField(max_length=32)
+    time = models.CharField(max_length=32)
+    prize = models.CharField(max_length=32)
+    price = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.race_text
+
