21b3c0ba4061916401d6ff77d96dc639162f3095
rafaelromon <rafaelromonsagredo@protonmail.com>
2019-04-06 13:34:16 +0200

    Commented out problematic lines in models.py

--- RaceOrganizerServer/api/models.py
+++ RaceOrganizerServer/api/models.py
@@ -14,34 +14,34 @@ class User(models.Model):
     isRunner = models.BooleanField(default=False)
     isVolunteer = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.user_text
-
-    def create_race(self):
-        return self.race
-    def update_race_information(self):
-        return self.info
-    def register_as_user(self):
-        return self.user
-    def delete_from_race(self):
-        return self.race
-    def sign_up_for_a_task(self):
-        return self.task
-    def check_race_details(self):
-        return self.race
-    def set_up_for_a_race(self):
-        return self.race
-
+    # TODO this does not work
+    # def __str__(self):
+    #     return self.user_text
+    #
+    # def create_race(self):
+    #     return self.race
+    # def update_race_information(self):
+    #     return self.info
+    # def register_as_user(self):
+    #     return self.user
+    # def delete_from_race(self):
+    #     return self.race
+    # def sign_up_for_a_task(self):
+    #     return self.task
+    # def check_race_details(self):
+    #     return self.race
+    # def set_up_for_a_race(self):
+    #     return self.race
 
 
 class Race(models.Model):
-    edition= models.CharField(max_length=32, unique=True)
+    edition = models.CharField(max_length=32, unique=True)
     sponsor = models.CharField(max_length=32)
     place = models.CharField(max_length=32)
     time = models.CharField(max_length=32)
     prize = models.CharField(max_length=32)
     price = models.CharField(max_length=32)
 
-    def __str__(self):
-        return self.race_text
-
+    # TODO this does not work
+    # def __str__(self):
+    #     return self.race_text
