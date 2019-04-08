b0f0e066f3cf566fee0a2b109980538f46a213b9
RafaelRomon <rafaelromonsagredo@protonmail.com>
2019-04-08 12:57:36 +0200

    Bug fixes and cleanup

--- RaceOrganizerClient/src/main/java/pages/Main.java
+++ RaceOrganizerClient/src/main/java/pages/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         LoginGateway gw = new LoginGateway("test", "test");
+        System.out.println(gw.login());
         SignupGateway sw = new SignupGateway("usuario1234", "test", "test", "test",
                 "usuario123@test.com", "52", "1998-05-02");
         System.out.println(sw.signUp());
