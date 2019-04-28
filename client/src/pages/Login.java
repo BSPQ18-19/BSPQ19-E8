1fd4aa1e7b043961f6524df18d1d1ae3584087f6
Asier <asiers49@gmail.com>
2019-04-28 18:10:45 +0200

    login

--- client/src/pages/Login.java
+++ client/src/pages/Login.java
@@ -13,7 +13,12 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+
+import networking.LoginGateway;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
@@ -30,72 +35,75 @@ public class Login extends JFrame {
 	private JPanel contentPanel;
 	private JTextField txtUsername;
 	private JPasswordField passwordField;
-	
-
 
 	/**
 	 * Create the frame.
 	 */
 	public Login() {
 		setBounds(100, 100, 650, 400);
-		contentPanel=new JPanel();
-		contentPanel.setLayout(new CardLayout(0,0));
+		contentPanel = new JPanel();
+		contentPanel.setLayout(new CardLayout(0, 0));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPanel);
-		
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(102, 153, 204));
 		panel.setLayout(null);
 		contentPanel.add(panel);
-		
-		JPanel panel_1 =new JPanel();
+
+		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new LineBorder(Color.WHITE));
 		panel_1.setOpaque(false);
 		panel_1.setBackground(Color.WHITE);
-		panel_1.setBounds(300,100,280,203);
+		panel_1.setBounds(300, 100, 280, 203);
 		panel.add(panel_1);
 		panel_1.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel_2 = new JPanel();
 		panel_2.setOpaque(false);
 		panel_1.add(panel_2, BorderLayout.SOUTH);
-		
+
 		JButton btnLogin = new JButton("Login");
 		panel_2.add(btnLogin);
 		btnLogin.setBackground(new Color(255, 255, 255));
 		btnLogin.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				Login.this.setVisible(false);
-				Dashboard das=new Dashboard();
-				das.setVisible(true);
+				//LoginGateway lgw = new LoginGateway(txtUsername.getText(), String.valueOf(passwordField.getPassword()));
+				//if (lgw.login()) {
+					Login.this.setVisible(false);
+					Dashboard das = new Dashboard();
+					das.setVisible(true);
+				//} else {
+				//	JOptionPane.showInternalMessageDialog(Login.this, "Username or password is incorrect");
+				//}
+
 			}
 		});
-		
+
 		JButton btnNewButton_1 = new JButton("Register");
 		panel_2.add(btnNewButton_1);
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Login.this.setVisible(false);
-				Registration res=new Registration(Login.this);
+				Registration res = new Registration(Login.this);
 				res.setVisible(true);
 			}
 		});
 		btnNewButton_1.setBackground(new Color(255, 255, 255));
-		
+
 		JPanel panel_3 = new JPanel();
 		panel_3.setOpaque(false);
 		panel_1.add(panel_3, BorderLayout.CENTER);
 		GridBagLayout gbl_panel_3 = new GridBagLayout();
-		gbl_panel_3.columnWidths = new int[] {30, 35, 206, 0, 30};
-		gbl_panel_3.rowHeights = new int[] {30, 45, 30, 30, 40};
-		gbl_panel_3.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel_3.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0};
+		gbl_panel_3.columnWidths = new int[] { 30, 35, 206, 0, 30 };
+		gbl_panel_3.rowHeights = new int[] { 30, 45, 30, 30, 40 };
+		gbl_panel_3.columnWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel_3.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0 };
 		panel_3.setLayout(gbl_panel_3);
-		
+
 		JLabel lblUserLogin = new JLabel("User Login");
 		lblUserLogin.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		GridBagConstraints gbc_lblUserLogin = new GridBagConstraints();
@@ -104,7 +112,7 @@ public class Login extends JFrame {
 		gbc_lblUserLogin.gridx = 2;
 		gbc_lblUserLogin.gridy = 1;
 		panel_3.add(lblUserLogin, gbc_lblUserLogin);
-		
+
 		JLabel lblUser = new JLabel("");
 		lblUser.setIcon(new ImageIcon(Login.class.getResource("/icons/user.png")));
 		GridBagConstraints gbc_lblUser = new GridBagConstraints();
@@ -113,7 +121,7 @@ public class Login extends JFrame {
 		gbc_lblUser.gridx = 1;
 		gbc_lblUser.gridy = 2;
 		panel_3.add(lblUser, gbc_lblUser);
-		
+
 		txtUsername = new JTextField();
 		txtUsername.setToolTipText("Username");
 		txtUsername.setBackground(new Color(245, 245, 245));
@@ -125,7 +133,7 @@ public class Login extends JFrame {
 		gbc_txtUsername.gridy = 2;
 		panel_3.add(txtUsername, gbc_txtUsername);
 		txtUsername.setColumns(10);
-		
+
 		JLabel lblPassword = new JLabel("");
 		lblPassword.setIcon(new ImageIcon(Login.class.getResource("/icons/key.png")));
 		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
@@ -134,7 +142,7 @@ public class Login extends JFrame {
 		gbc_lblPassword.gridx = 1;
 		gbc_lblPassword.gridy = 3;
 		panel_3.add(lblPassword, gbc_lblPassword);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setToolTipText("Password");
 		passwordField.setBackground(new Color(245, 245, 245));
@@ -145,19 +153,19 @@ public class Login extends JFrame {
 		gbc_passwordField.gridx = 2;
 		gbc_passwordField.gridy = 3;
 		panel_3.add(passwordField, gbc_passwordField);
-		
+
 		JLabel lblicon = new JLabel("");
 		lblicon.setBounds(12, 37, 302, 287);
 		panel.add(lblicon);
 		lblicon.setHorizontalAlignment(SwingConstants.CENTER);
 		lblicon.setIcon(new ImageIcon(Login.class.getResource("/icons/icon.png")));
-		
+
 		JLabel lblRaceOrganizer = new JLabel("RACE ORGANIZER");
 		lblRaceOrganizer.setFont(new Font("Tahoma", Font.BOLD, 22));
 		lblRaceOrganizer.setBounds(339, 52, 230, 27);
 		panel.add(lblRaceOrganizer);
-		
+
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		
+
 	}
 }
