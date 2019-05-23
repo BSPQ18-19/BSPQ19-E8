ef7472eb0ee7b403380c3a72c0ba67c5fda5c0a5
rafaelromon <rafaelromonsagredo@protonmail.com>
2019-05-23 18:40:02 +0200

    gitignore fix

--- doxygen/RaceOrganizerServer/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jailander/Cloud/Github/BSPQ19-E8/RaceOrganizerServer/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
